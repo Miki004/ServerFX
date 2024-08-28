ba55c7c7775c828087076ee4d1c70f2581ce61c6
stefano7703 <rosarioalise7@gmail.com>
2024-08-28 15:47:29 +0200

    ecco a lei alcune cose
    mio signore

--- src/main/java/com/example/serverfx/ServerApp.java
+++ src/main/java/com/example/serverfx/ServerApp.java
@@ -7,9 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ServerApp extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
+        stage = stage;
+        stage.setTitle("H-CLUS-Server");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/serverfx/StartServer.fxml"));
         Parent root = loader.load();
         stage.setScene(new Scene(root));
